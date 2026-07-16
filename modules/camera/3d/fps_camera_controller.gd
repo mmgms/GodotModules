1337ade20c2fbe7a4aa353698ba40ac872a6738a
mmgms <mmgms0001@gmail.com>
2026-07-16 11:35:48 +0200

    simplify setup of fps camera

--- modules/camera/3d/fps_camera_controller.gd
+++ modules/camera/3d/fps_camera_controller.gd
@@ -2,6 +2,8 @@ extends Node
 class_name FPSCameraController3D
 ## Updates camera rig based on input callback, needs pitch yaw camera rig
 ## can be used for third person add a springarm3d as a parent of camera and set spring length as the camera distance
+## can set custom input callback or use built in mouse_input_callback and call handle_input in _unhandled_input
+## call update every frame
 
 @export_category("Nodes")
 @export var pitch: Node3D
@@ -18,12 +20,26 @@ var _rotation: Vector3
 
 var _input_callback: Callable
 
+var _mouse_input: Vector2
+var _mouse_sensitivity: float
 
 ## () -> Vector2
 func set_input_callback(cb: Callable):
 	_input_callback = cb
 	return self
+	
+func set_mouse_input_callback(mouse_sensitivity: float = 0.002):
+	_mouse_sensitivity = mouse_sensitivity
+	_input_callback = _get_mouse_input
+	return self
 
+func handle_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		_mouse_input.x = -event.screen_relative.x * _mouse_sensitivity
+		_mouse_input.y = -event.screen_relative.y * _mouse_sensitivity
+
+func _get_mouse_input() -> Vector2:
+	return _mouse_input
 
 func update(_delta: float):
 	var input: Vector2 = _input_callback.call()
@@ -36,3 +52,5 @@ func update(_delta: float):
 
 	var _camera_rotation = Vector3(_rotation.x, 0, 0)
 	pitch.transform.basis = Basis.from_euler(_camera_rotation)
+	
+	_mouse_input = Vector2.ZERO
