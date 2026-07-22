49aa5b7866378cbf36b3c6ef95020bb13a2cb498
mmgms <mmgms0001@gmail.com>
2026-07-22 12:22:44 +0200

    grid placement rotations

--- modules/grid_placement/grid_placement.gd
+++ modules/grid_placement/grid_placement.gd
@@ -1,13 +1,14 @@
 extends Node
 
+var _rotations = [0, -PI/2, PI,  PI/2]
 class SlotInfo:
 	var is_occupied: bool
 	var item_placement: ItemPlacement
 	
 class ItemPlacement:
 	var item: ItemInfo
-	var position: Vector2i
-	var rotation_rad: float 
+	var position_center: Vector2
+	var rotation: int 
 
 class ItemInfo:
 	var item: Variant
@@ -85,11 +86,10 @@ func _get_top_left_position_from_center(pos: Vector2, extents: Vector2i):
 func _place_item(idx: int, position_center: Vector2):
 	var item = _all_items[idx]
 
-	var pos_top_left_grid_coord = Vector2i(_get_top_left_position_from_center(position_center, item.grid.get_size()))
-	if not _check_can_place(item.info, pos_top_left_grid_coord, 0):
+	if not _check_can_place(item.info, position_center, 0):
 		return
 
-	var item_placement = _add_item_placed(item.info, pos_top_left_grid_coord, 0)
+	var item_placement = _add_item_placed(item.info, position_center, 0)
 
 	var instance = slot_item_scene.instantiate() as SlotItemScene
 	slot_items_container.add_child(instance)
@@ -111,6 +111,7 @@ var currently_dragged_scene: SlotItemScene
 var currently_dragged_item_placement: ItemPlacement
 var prev_center_pos: Vector2
 var mouse_offset: Vector2
+var current_rotation_idx: int
 
 func _get_pos_from_grid_idx(idx):
 	return (Vector2(idx) + Vector2.ONE * 0.5) * _slot_size + grid_container.position
@@ -132,6 +133,7 @@ func _physics_process(_delta: float) -> void:
 				prev_center_pos = currently_dragged_scene.position
 				mouse_offset = prev_center_pos - slot_items_container.get_local_mouse_position()
 				is_dragging = true
+				current_rotation_idx = currently_dragged_item_placement.rotation
 				_remove_item_placement(currently_dragged_item_placement)
 	else:
 		var mouse_pos = slot_items_container.get_local_mouse_position()
@@ -142,21 +144,38 @@ func _physics_process(_delta: float) -> void:
 		var offset_norm = ((currently_dragged_scene.position - prev_center_pos)/_slot_size).snapped(Vector2.ONE)
 		var prev_center_norm = (prev_center_pos - grid_container.position)/_slot_size
 
-		var new_center_norm = prev_center_norm + offset_norm
 
-		var test_pos_grid_idx = Vector2i(_get_top_left_position_from_center(new_center_norm, item.grid_filled.get_size()))
+		var reg_extents = item.grid_filled.get_size()
+		var new_center_norm 
+		if reg_extents.x == reg_extents.y:
+			new_center_norm = prev_center_norm + offset_norm
+		else:
+			var extents = reg_extents
+			var flipped_extents = Vector2i(reg_extents.y, reg_extents.x)
+			if current_rotation_idx % 2 == 1:
+				extents = flipped_extents
+			var extents_center = extents/2.0
+			var grid_offset = Vector2(fposmod(extents_center.x,1), fposmod(extents_center.y,1))
+			var rel_pos_norm = (currently_dragged_scene.position - grid_container.position)/_slot_size
+			new_center_norm = (rel_pos_norm - grid_offset).snapped(Vector2.ONE) + grid_offset
 
-		var can_place = _check_can_place(item, test_pos_grid_idx, 0)
+			pass
+
+		var can_place = _check_can_place(item, new_center_norm, current_rotation_idx)
 		DebugDraw2D.set_text("can_place", can_place)
-		#MyDebugDraw2d.point(grid_container.position + new_center_norm * _slot_size, _delta)
+		MyDebugDraw2d.point(grid_container.position + new_center_norm * _slot_size, _delta, Color.REBECCA_PURPLE, 2.0)
+		if Input.is_action_just_pressed("rotate"):
+			current_rotation_idx = wrapi(current_rotation_idx+1, 0, _rotations.size())
+			currently_dragged_scene.rotation = _rotations[current_rotation_idx]
 
 		if Input.is_action_just_released("click"):
 			if can_place:
-				var new_placement = _add_item_placed(item, test_pos_grid_idx, 0)
+				var new_placement = _add_item_placed(item, new_center_norm, current_rotation_idx)
 				currently_dragged_scene.position = grid_container.position + new_center_norm * _slot_size
 				_scene_to_item_placement[currently_dragged_scene] = new_placement
 			else:
-				var new_placement = _add_item_placed(item, currently_dragged_item_placement.position, 0.0)
+				var new_placement = _add_item_placed(item, currently_dragged_item_placement.position_center, currently_dragged_item_placement.rotation)
+				currently_dragged_scene.rotation = _rotations[currently_dragged_item_placement.rotation]
 				currently_dragged_scene.position = prev_center_pos
 				_scene_to_item_placement[currently_dragged_scene] = new_placement
 			currently_dragged_scene.set_enabled(true)
@@ -164,31 +183,42 @@ func _physics_process(_delta: float) -> void:
 			is_dragging = false
 
 
-func _check_can_place(item: ItemInfo, position_top_left: Vector2i, rotation: int):
+func _get_grid_idx_from_center_and_rotation(extents: Vector2i, rel_grid_idx: Vector2i, position_center: Vector2, rotation: int):
 
+	var rel_center = Vector2(extents)/2.0
+	var vec_to_rel_grid_idx = Vector2(rel_grid_idx) + Vector2.ONE/2 - rel_center
+	var rotated_vec_to_rel_grid_idx = vec_to_rel_grid_idx.rotated(_rotations[rotation])
+	return Vector2i((position_center + rotated_vec_to_rel_grid_idx))
+
+func _check_can_place(item: ItemInfo, position_center: Vector2, rotation: int):
+
+	var occupied_spot_found = false
 	for data in item.grid_filled:
 		if not data.data:
 			continue
 		var pos = data.point
-		var final_pos = pos + position_top_left
+		var final_pos = _get_grid_idx_from_center_and_rotation(item.grid_filled.get_size(), pos, position_center, rotation)
 		if not _grid.is_in_bounds_veci(final_pos) or _grid.get_at_veci(final_pos).is_occupied:
-			return false
-	return true
+			MyDebugDraw2d.point(_get_pos_from_grid_idx(final_pos), get_physics_process_delta_time(), Color.RED)
+			occupied_spot_found = true
+			continue
+		MyDebugDraw2d.point(_get_pos_from_grid_idx(final_pos), get_physics_process_delta_time(), Color.GREEN)
+	return not occupied_spot_found
 
 var _items_placed: Array[ItemPlacement]
-func _add_item_placed(item: ItemInfo, position_top_left: Vector2i, rotation: int):
+func _add_item_placed(item: ItemInfo, position_center: Vector2, rotation: int):
 	var item_placement: ItemPlacement = ItemPlacement.new()
 	item_placement.item = item
 
-	item_placement.position = position_top_left
-	item_placement.rotation_rad = rotation
+	item_placement.position_center = position_center
+	item_placement.rotation = rotation
 	_items_placed.append(item_placement)
 
 	for data in item_placement.item.grid_filled:
 		if not data.data:
 			continue
 		var pos = data.point
-		var final_pos = pos + position_top_left
+		var final_pos = _get_grid_idx_from_center_and_rotation(item.grid_filled.get_size(), pos, position_center, rotation)
 
 		var slot_info = SlotInfo.new()
 		slot_info.is_occupied = true
@@ -202,9 +232,14 @@ func _remove_item_placement(placement: ItemPlacement):
 		if not data.data:
 			continue
 		var pos = data.point
-		var final_pos = pos + placement.position
+		var final_pos = _get_grid_idx_from_center_and_rotation(placement.item.grid_filled.get_size(), pos, placement.position_center, placement.rotation)
 		#MyDebugDraw2d.point(_get_pos_from_grid_idx(final_pos), 1.0, Color.ORANGE)
 
 		_grid.set_at_veci(final_pos, SlotInfo.new())
 
 	_items_placed.erase(placement)
+
+
+func _get_rotated_veci(x, y, rot):
+	var res = Vector2i(Vector2(x, y).rotated(_rotations[rot]).round())
+	return res
