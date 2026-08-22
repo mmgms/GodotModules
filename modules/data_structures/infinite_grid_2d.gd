e4d491e4d94bb3569915f65e8af3a7d39e90f3f9
mmgms <mmgms0001@gmail.com>
2026-08-22 20:09:28 +0200

    infinire grid

---
+++ modules/data_structures/infinite_grid_2d.gd
@@ -0,0 +1,34 @@
+extends RefCounted
+class_name InfiniteGrid2D
+
+var _data: Dictionary[Vector2i, Variant]
+
+func _init() -> void:
+	_data = {}
+
+func get_at_veci(vec: Vector2i) -> Variant:
+	if not _data.has(vec):
+		return null
+	return _data[vec]
+
+func set_at_veci(vec: Vector2i, value: Variant):
+	if value == null:
+		_data.erase(vec)
+		return
+	_data[vec] = value
+	return self
+	
+	
+func get_array():
+	return _data.values()
+	
+func get_points():
+	return _data.keys()
+
+
+func get_neighbours_4(pos: Vector2i) -> Array[Vector2i]:
+	return MathUtils.get_neighbours_4(pos).filter(func(pos): return get_at_veci(pos) != null)
+
+func get_neighbours_8(pos: Vector2i) -> Array[Vector2i]:
+	return MathUtils.get_neighbours_8(pos).filter(func(pos): return get_at_veci(pos) != null)
+	
